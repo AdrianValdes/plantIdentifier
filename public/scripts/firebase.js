6fdd28627fdddf8ee683f36d48459fd30fc10323
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2020-06-16 15:55:15 +0200

    Update firebase.js

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: 'AIzaSyBhO1-0x1dNgq0OJn8BlcCdox55iI2nkXA',
-  authDomain: 'plantproject-2df69.firebaseapp.com',
-  databaseURL: 'https://plantproject-2df69.firebaseio.com',
-  projectId: 'plantproject-2df69',
-  storageBucket: 'plantproject-2df69.appspot.com',
-  messagingSenderId: '238576766779',
-  appId: '1:238576766779:web:7fb52369f42d4b1cedf11e',
-  measurementId: 'G-HMX47EXZR1',
+  apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  authDomain: xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  databaseURL: 'xxxxxxxxxxxxxxxxxx',
+  projectId: 'xxxxxxxxxxxx',
+  storageBucket: 'xxxxxxxxxxxxxxxxxxxxx',
+  messagingSenderId: 'xxxxxxxxxxxx',
+  appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  measurementId: 'xxxxxxxxxxx',
 };
 firebase.initializeApp(firebaseConfig);
 
